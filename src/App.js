17aeba6f7931a954af6e81633e71d72fb9b8e1a4
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2023-04-15 09:06:12 +0000

    youssef

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { flushSync } from "react-dom";
 import "./App.css";
 import axios from "axios";
 import LoadingSpinner from "./LoadingSpinner";
 
 import jsPDF from "jspdf";
-import "jspdf-autotable";
+import autoTable from "jspdf-autotable";
 
-function downloadPDF(text) {
-  // Create a new jsPDF instance
-  const doc = new jsPDF();
+function downloadPDF(text, txt_type) {
+  // create a new jsPDF instance
+  const doc = new jsPDF({
+    orientation: "portrait",
+    unit: "mm",
+    format: "a4",
+  });
 
-  // Add the text to the PDF document
-  doc.text(text, 10, 10);
+  // Add heading
+  doc.setFontSize(16);
+  doc.setFont("helvetica", "bold");
+  doc.text(txt_type.toUpperCase(), 10, 10);
+  //add text
+  doc.setLineWidth(0.5);
+  doc.setLineHeightFactor(1.2);
+  doc.setFontSize(12);
+  doc.setFont("times", "normal");
+  const splitText = doc.splitTextToSize(text, doc.internal.pageSize.width - 20);
+  let cursorY = 20; // set initial y position
+  splitText.forEach(line => {
+    if (cursorY > 280) { // check if the cursor is at the end of the page
+      doc.addPage(); // add new page
+      cursorY = 20; // reset y position
+    }
+    doc.text(line, 10, cursorY); // add line of text
+    cursorY += 10; // increase y position by line height
+  });
 
-  // Download the PDF document
-  doc.save("text-to-pdf.pdf");
+  // save the PDF document
+  doc.save(txt_type + ".pdf");
 }
 
 function App() {
@@ -28,14 +49,16 @@ function App() {
   const [pdfInput, setPdfInput] = useState();
   const [isPdfOutput, setIsPdfOutput] = useState(false);
   const [pdfName, setPdfName] = useState("Select a PDF");
-
   const [isStarted, setIsStarted] = useState(false);
   const [isOutput, setIsOutput] = useState(false);
   const [outputData, setOutputData] = useState("");
   const [summaryData, setSummaryData] = useState("");
   const [rephrasedData, setRephrasedData] = useState("");
   const [quizData, setQuizData] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [funcSelect, setFuncSelect] = useState("summary");
+  const [isSummaryLoading, setIsSummaryLoading] = useState(false);
+  const [isRephraseLoading, setIsRephraseLoading] = useState(false);
+  const [isQuizLoading, setIsQuizLoading] = useState(false);
 
   const start = () => {
     flushSync(() => {
@@ -98,6 +121,7 @@ function App() {
     setIsPdfInput(true);
   };
   const onPdfChange = (e) => {
+    e.preventDefault();
     setPdfInput(e.target.files[0]);
     setPdfName(e.target.files[0] ? e.target.files[0].name : "Select a PDF");
   };
@@ -122,7 +146,14 @@ function App() {
   };
 
   const getoutputs = async () => {
-    setIsLoading(true);
+    setSummaryData("");
+    setRephrasedData("");
+    setQuizData("");
+
+    setIsSummaryLoading(true);
+    setIsRephraseLoading(true);
+    setIsQuizLoading(true);
+
     await axios
       .post(
         "http://127.0.0.1:8080/api/text/summarize",
@@ -139,29 +170,9 @@ function App() {
       .then((res) => {
         flushSync(() => {
           setSummaryData(res.data);
-        });
-        const element = document.getElementById("output-section");
-        if (element) {
-          element.scrollIntoView({ behavior: "smooth" });
-        }
-      })
-      .catch((e) => console.log(e));
-    await axios
-      .post(
-        "http://127.0.0.1:8080/api/text/rephrase",
-
-        {
-          text: text,
-        },
-        {
-          headers: {
-            "content-type": "application/x-www-form-urlencoded",
-          },
-        }
-      )
-      .then((res) => {
-        flushSync(() => {
-          setRephrasedData(res.data);
+          setFuncSelect("summary");
+          setOutputData(res.data);
+          setIsSummaryLoading(false);
         });
         const element = document.getElementById("output-section");
         if (element) {
@@ -185,6 +196,7 @@ function App() {
       .then((res) => {
         flushSync(() => {
           setQuizData(res.data);
+          setIsQuizLoading(false);
         });
         const element = document.getElementById("output-section");
         if (element) {
@@ -192,26 +204,63 @@ function App() {
         }
       })
       .catch((e) => console.log(e));
-    setIsLoading(false);
+    await axios
+      .post(
+        "http://127.0.0.1:8080/api/text/rephrase",
+
+        {
+          text: text,
+        },
+        {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((res) => {
+        flushSync(() => {
+          setRephrasedData(res.data);
+          setIsRephraseLoading(false);
+        });
+        const element = document.getElementById("output-section");
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      })
+      .catch((e) => console.log(e));
   };
 
   const createSummaryTextarea = () => {
     setOutputData(summaryData);
+    setFuncSelect("summary");
   };
   const createRephrasedTextarea = () => {
     setOutputData(rephrasedData);
+    setFuncSelect("rephrase");
   };
   const createQuizTextarea = () => {
     setOutputData(quizData);
+    setFuncSelect("quiz");
   };
 
   const download = () => {
-    downloadPDF("TEXT");
+    downloadPDF(outputData, funcSelect);
+  };
+  const downloadAll = () => {
+    downloadPDF(
+      "Summary :\n" +
+        summaryData +
+        "\n\nRephrased :\n" +
+        rephrasedData +
+        "\n\nQuiz :\n" +
+        quizData,
+      "Quizter"
+    );
   };
 
   return (
     <div className="App">
-      <div className="started_page">
+      <div className="started_page" id="started-page">
         <img src="img/v1_9.png" alt="My Image" className="img_title" />
         <div className="landing_bubble_container">
           <div className="landing_bubble">
@@ -311,7 +360,9 @@ function App() {
             </button>
           </div>
           <div className="OutputContainer">
-            {isLoading ? (
+            {(isSummaryLoading && funcSelect === "summary") ||
+            (isRephraseLoading && funcSelect === "rephrase") ||
+            (isQuizLoading && funcSelect === "quiz") ? (
               <LoadingSpinner />
             ) : (
               <textarea
@@ -329,7 +380,7 @@ function App() {
             <button className="download_btn" onClick={download}>
               DOWNLOAD
             </button>
-            <button className="download_btn" onClick={download}>
+            <button className="download_btn" onClick={downloadAll}>
               DOWNLOAD ALL
             </button>
           </div>
